d6a0bbfc6b519f76400aaf45a4aeb9b3887c9a16
Plugin <tja414312570@gmail.com>
2023-04-21 17:56:03 +0800

    支持spring和spring boot引导

--- src/main/java/com/github/monkeywie/proxyee/spring/SpringProxyStandardEnvironment.java
+++
@@ -1,6 +0,0 @@
-package com.github.monkeywie.proxyee.spring;
-
-import org.springframework.core.env.StandardEnvironment;
-
-public class SpringProxyStandardEnvironment extends StandardEnvironment {
-}
